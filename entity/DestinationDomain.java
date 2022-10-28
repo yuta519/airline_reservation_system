17df816f08e65ef6503bf45d7578cbaf64b7de1b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-28 11:29:06 +0900

    change dir name from domain to entity

---
+++ entity/DestinationDomain.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class DestinationDomain {
+    public String type;
+    public String subType;
+    public String name;
+    public String iataCode;
+
+    public DestinationDomain(String type, String subType, String name, String iataCode) {
+        this.type = type;
+        this.subType = subType;
+        this.name = name;
+        this.iataCode = iataCode;
+    }
+}
